6dadc20f6b85b8ba435845ab77cf88440f4b0fab
jaganaram <jaganaramprasad333@gmail.com>
2022-01-05 16:17:34 +0530

    prase message

--- hw6_social.py
+++ hw6_social.py
@@ -88,7 +88,19 @@ Parameters: str
 Returns: list of strs
 '''
 def findHashtags(message):
-    return
+    new=message.split("#")
+    r=""
+    list=[]
+    for i in new[1:]:
+        for j in i:
+            # print(j)
+            if j not in endChars:
+                r+=j
+            else:
+                break
+        list.append('#'+r)
+        r=""
+    return list
 
 
 '''
@@ -292,6 +304,7 @@ if __name__ == "__main__":
     test.testParseName()
     test.testParsePosition()
     test.testParseState()
+    test.testFindHashtags()
 
     ## Uncomment these for Week 2 ##
     """print("\n" + "#"*15 + " WEEK 2 TESTS " +  "#" * 16 + "\n")
